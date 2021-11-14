8e341eb3762f4c627130204edb457a33a7f7574f
Padmavathi2021 <93919150+Padmavathi2021@users.noreply.github.com>
2021-11-14 16:30:07 +0530

    Update PageObjectManager.java

--- src/main/java/managers/PageObjectManager.java
+++ src/main/java/managers/PageObjectManager.java
@@ -4,14 +4,10 @@ import org.openqa.selenium.WebDriver;
 import pageObjects.BasePage;
 import pageObjects.FaceBookHomePage;
 import pageObjects.FaceBookLoginPage;
-import pageObjects.WalletHubProfilePage;
-import pageObjects.WalletHubReviewPage;
 
 public class PageObjectManager extends BasePage{
 	private FaceBookLoginPage faceBookLoginPage;
 	private FaceBookHomePage faceBookHomePage;
-	private WalletHubReviewPage walletHubReviewPage;
-	private WalletHubProfilePage walletHubProfilePage;
 	
 	public PageObjectManager(WebDriver driver) {
 		super(driver);
@@ -23,13 +19,5 @@ public class PageObjectManager extends BasePage{
 
 	public FaceBookHomePage getFaceBookHomePage(){
 		return (faceBookHomePage == null) ? faceBookHomePage = new FaceBookHomePage(driver) : faceBookHomePage;
-	}
-	
-	public WalletHubReviewPage getWalletHubReviewPage(){
-		return (walletHubReviewPage == null) ? walletHubReviewPage = new WalletHubReviewPage(driver) : walletHubReviewPage;
-	}
-	
-	public WalletHubProfilePage getWalletHubProfilePage(){
-		return (walletHubProfilePage == null) ? walletHubProfilePage = new WalletHubProfilePage(driver) : walletHubProfilePage;
 	}
 }
